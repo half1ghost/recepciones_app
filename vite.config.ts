bb36bfbddf63a4c21314821dfa88468597bd4454
TioJonah <night1hack@gmail.com>
2025-12-15 00:31:31 -0300

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/recepciones_app/', // 👈 obligatorio para GitHub Pages
   plugins: [react()],
-  server: { port: 5173 },
-});
+})
